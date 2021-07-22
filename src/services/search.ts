198d0ae691ed9815fb88043c5df476c1d71ab8b8
cruxcode <swaroopshyam0@gmail.com>
2021-07-22 00:12:03 -0400

    added search and summary endpoint

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,1 +1,32 @@
-export function search(query: string) {}
+export function search(
+	query: string,
+	lang: string,
+	source: string,
+	size: number
+) {
+	const headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	return fetch("http://3.129.3.172:8080/search", {
+		method: "POST",
+		headers: headers,
+		body: JSON.stringify({ query, lang, source, size }),
+	}).then((resp) => resp.json());
+}
+
+export function summary(
+	queryid: string,
+	filename: string,
+	lang: string,
+	source: string
+) {
+	return fetch(
+		"http://3.129.3.172:8080/search/summary?" +
+			`queryid=${queryid}` +
+			`&filename=${filename}` +
+			`&lang=${lang}` +
+			`&source=${source}`,
+		{
+			method: "GET",
+		}
+	).then((resp) => resp.json());
+}
